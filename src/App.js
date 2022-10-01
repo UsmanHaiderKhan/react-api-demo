68f3b50c0372a5ac9c002ac16378eae2f44d70e7
Usman Haider Khan <37379001+UsmanHaiderKhan@users.noreply.github.com>
2022-10-01 18:55:35 +0500

    🎉 Add : Fetch data from API URL and show in render method

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
 import './App.css';
+// import LoginForm from './components/auth/login';
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+// const App = () =>{
+// <div>
+//   <LoginForm/>
+// </div>
+// }
 
+
+class App extends Component{
+  
+  constructor(props){
+    super(props);
+    this.state = {
+      items : [],
+      isLoad: false
+    }
+  }
+  componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/posts')
+        .then(res => res.json())
+        .then(json => {
+          this.setState({
+            isLoad: true,
+            items:json
+          });
+        });
+  }
+  render(){
+    var {items, isLoad } = this.state;
+    if(!isLoad){
+      return <div>Data has not been loaded</div>
+    } else {
+      return(
+        <div className="container">
+          <div className='row justify-content-center mt-3'>
+            <h2 className='text-center'>Fetched Data From API</h2> 
+                {items.map((item) => (
+                <div className='col-md-6 p-3' key={item.id}>
+                 <div className="card">
+                  <div className="card-body">
+                    <h5 className="card-title">{item.title}</h5>
+                    <p className="card-text">{item.body}</p>
+                    <a href='#' className='btn btn-outline-primary'>Visit us</a>
+                   </div>
+                   </div>
+                  </div>
+                ))}
+            </div>
+          </div>
+      );
+      }
+    }
+  }
 export default App;
